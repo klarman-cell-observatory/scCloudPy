7b475eab5bd221e311e97aa64167718709ea3ca2
jgould <jgould@broadinstitute.org>
2019-07-29 16:39:21 -0400

    mac/linux tests

--- tests/test_cluster_pipeline.py
+++ tests/test_cluster_pipeline.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import scCloud as sc
 import scCloud.commands
 from .test_util import assert_adata_files_equal
 
@@ -15,14 +16,17 @@ class TestClusterPipeline(unittest.TestCase):
              '--run-approximated-leiden', '--run-tsne', '--run-umap',
              '--run-net-tsne', '--run-net-fitsne', '--run-net-umap', '--run-fitsne'])
         cmd.execute()
-        assert_adata_files_equal(self, os.path.join('tests', 'output', 'test_cluster.h5ad'), 'test_cluster.h5ad')
-
-
-# '--run-approximated-louvain',
-# '--run-louvain',
-# '--run-fle',
-# '--run-net-fle'
-# '--plot-hvg',
+        assert_adata_files_equal(self, os.path.join('tests', 'output', 'test_cluster.h5ad'), 'test_cluster.h5ad',
+            obs_blacklist=set(['approx_leiden_labels']))
+        # approx_leiden_labels for 3rd cell differs between mac and linux
+        test_data = sc.tools.read_input('test_cluster.h5ad', mode='a')
+        data = sc.tools.read_input(os.path.join('tests', 'output', 'test_cluster.h5ad'), mode='a')
+        self.assertEqual((data.obs['approx_leiden_labels'] != test_data.obs['approx_leiden_labels']).sum(), 1)
+        # '--run-approximated-louvain',
+        # '--run-louvain',
+        # '--run-fle',
+        # '--run-net-fle'
+        # '--plot-hvg',
 
 
 if __name__ == '__main__':
--- tests/test_util.py
+++ tests/test_util.py
@@ -12,35 +12,54 @@ def assert_excel_equal(test_case, path, test_path):
         pd.testing.assert_frame_equal(test_output[key], output[key])
 
 
-def assert_dict_of_arrays_equal(test_case, dict1, dict2):
-    test_case.assertListEqual(list(dict1.keys()), list(dict2.keys()))
-
+def assert_dict_of_arrays_equal(test_case, dict1, dict2, blacklist):
+    if blacklist is None:
+        blacklist = set()
+    list1_keys = set(dict1.keys())
+    list2_keys = set(dict2.keys())
+    test_case.assertSetEqual(list1_keys.difference(blacklist), list2_keys.difference(blacklist))
     for key in dict1.keys():
-        val1 = dict1[key]
-        val2 = dict2[key]
-        if scipy.sparse.issparse(val1):
-            val1 = val1.toarray()
-        if scipy.sparse.issparse(val2):
-            val2 = val2.toarray()
-        if isinstance(val1, np.ndarray):
-            np.testing.assert_array_almost_equal(val1, val2, err_msg=str(key) + ' not equal')
-        else:
-            np.testing.assert_array_equal(val1, val2, err_msg=str(key) + ' not equal')
+        if key not in blacklist:
+            val1 = dict1[key]
+            val2 = dict2[key]
+            if scipy.sparse.issparse(val1):
+                val1 = val1.toarray()
+            if scipy.sparse.issparse(val2):
+                val2 = val2.toarray()
+            if isinstance(val1, np.ndarray):
+                np.testing.assert_array_almost_equal(val1, val2, err_msg=str(key) + ' not equal')
+            else:
+                np.testing.assert_array_equal(val1, val2, err_msg=str(key) + ' not equal')
 
 
-def assert_adata_files_equal(test_case, path, test_path):
+def assert_adata_files_equal(test_case, path, test_path, obs_blacklist=None, var_blacklist=None, uns_blacklist=None,
+                             obsm_blacklist=None):
     test_data = sc.tools.read_input(test_path, mode='a')
     data = sc.tools.read_input(path, mode='a')
-    assert_adata_equal(test_case, data, test_data)
+    assert_adata_equal(test_case, data, test_data, obs_blacklist, var_blacklist, uns_blacklist, obsm_blacklist)
 
 
-def assert_adata_equal(test_case, data1, data2):
+def assert_adata_equal(test_case, data1, data2, obs_blacklist=None, var_blacklist=None, uns_blacklist=None,
+                       obsm_blacklist=None):
     if scipy.sparse.issparse(data1.X):
         data1.X = data1.X.toarray()
     if scipy.sparse.issparse(data2.X):
         data2.X = data2.X.toarray()
     np.testing.assert_array_almost_equal(data1.X, data2.X)
-    pd.testing.assert_frame_equal(data1.obs, data2.obs)
-    pd.testing.assert_frame_equal(data1.var, data2.var)
-    assert_dict_of_arrays_equal(test_case, data1.uns, data2.uns)
-    assert_dict_of_arrays_equal(test_case, data1.obsm, data2.obsm)
+    if obs_blacklist is not None:
+        obs1 = data1.obs.drop(obs_blacklist, axis=1)
+        obs2 = data2.obs.drop(obs_blacklist, axis=1)
+    else:
+        obs1 = data1.obs
+        obs2 = data2.obs
+
+    pd.testing.assert_frame_equal(obs1, obs2)
+    if var_blacklist is not None:
+        var1 = data1.var.drop(var_blacklist, axis=1)
+        var2 = data2.var.drop(var_blacklist, axis=1)
+    else:
+        var1 = data1.var
+        var2 = data2.var
+    pd.testing.assert_frame_equal(var1, var2)
+    assert_dict_of_arrays_equal(test_case, data1.uns, data2.uns, uns_blacklist)
+    assert_dict_of_arrays_equal(test_case, data1.obsm, data2.obsm, obsm_blacklist)
