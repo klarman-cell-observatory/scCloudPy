4f444bb9dc51a0b2e40bb9351cbfa50a709097f3
jgould <jgould@broadinstitute.org>
2019-07-30 09:02:11 -0400

    hvg_rank on different platforms

--- tests/test_cluster_pipeline.py
+++ tests/test_cluster_pipeline.py
@@ -17,12 +17,14 @@ class TestClusterPipeline(unittest.TestCase):
              '--run-net-tsne', '--run-net-fitsne', '--run-net-umap', '--run-fitsne'])
         cmd.execute()
         assert_adata_files_equal(self, os.path.join('tests', 'output', 'test_cluster.h5ad'), 'test_cluster.h5ad',
-            obs_blacklist=set(['approx_leiden_labels', 'leiden_labels']))
+            obs_blacklist=set(['approx_leiden_labels']), var_blacklist=set(['hvg_rank']))
         # approx_leiden_labels for 3rd cell differs between mac and linux
         test_data = sc.tools.read_input('test_cluster.h5ad', mode='a')
         data = sc.tools.read_input(os.path.join('tests', 'output', 'test_cluster.h5ad'), mode='a')
-        self.assertLessEqual((data.obs['approx_leiden_labels'] != test_data.obs['approx_leiden_labels']).sum(), 1)
-        self.assertLessEqual((data.obs['leiden_labels'] != test_data.obs['leiden_labels']).sum(), 1)
+        n = (data.obs['approx_leiden_labels'] != test_data.obs['approx_leiden_labels']).sum()
+        self.assertLessEqual(n, 1, '{} approx_leiden_labels differ'.format(n))
+        n = (data.var['hvg_rank'] != test_data.var['hvg_rank']).sum()
+        self.assertLessEqual(n, 1, '{} hvg_rank differ'.format(n))
         # '--run-approximated-louvain',
         # '--run-louvain',
         # '--run-fle',
